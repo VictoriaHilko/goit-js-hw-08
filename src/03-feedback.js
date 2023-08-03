42dda9bd88d6516d6a1cd05c58424cf528e86f9e
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2023-08-03 11:42:51 +0300

    Update 03-feedback.js

--- src/03-feedback.js
+++ src/03-feedback.js
@@ -2,38 +2,46 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 
+let data;
+
 //1
 form.addEventListener('input', throttle(onInput, 500));
 
 const currentData = JSON.parse(localStorage.getItem('feedback-form-state'));
 
+if(currentData){
+    form.email.value = currentData.email;
+    form.message.value = currentData.message;
+
+    data = {
+        email_data: form.email.value,
+        message_data: form.message.value
+    };
+}
+
 function onInput() {
 
-    form.elements.email.value = currentData.email;
-    form.elements.message.value = currentData.message;
-
     const data = {
-        email_data: form.elements.email.value,
-        message_data: form.elements.message.value
+        email_data: form.email.value,
+        message_data: form.message.value
     };
-
     localStorage.setItem('feedback-form-state', JSON.stringify(data));
 }
 
 //2
-window.addEventListener('load', onLoad);
+// window.addEventListener('load', onLoad);
 
-function onLoad() {
-    // const getformData = JSON.parse(localStorage.getItem('feedback-form-state'));
+// function onLoad() {
+//     // const getformData = JSON.parse(localStorage.getItem('feedback-form-state'));
 
-    if (getformData) {
-        form.elements.email.value = getformData.email_data;
-        form.elements.message.value = getformData.message_data;
+//     if (getformData) {
+//         form.elements.email.value = getformData.email_data;
+//         form.elements.message.value = getformData.message_data;
 
-        // console.log(data);
-    }
-    console.log(data);
-}
+//         // console.log(data);
+//     }
+//     console.log(data);
+// }
 
 //3
 form.addEventListener('submit', onSubmit);
