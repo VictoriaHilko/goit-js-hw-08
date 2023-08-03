71221cb145e971fe2856e45b6193278241d2e05b
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2023-08-03 12:07:19 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,51 +2,41 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 
-let email_value= '';
-let message_value = '';
-
-const data = {
-    email_data: email_value,
-    message_data: message_value
-};
+let data;
 
 //1
 form.addEventListener('input', throttle(onInput, 500));
 
+const currentData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if(currentData){
+    form.email.value = currentData.email;
+    form.message.value = currentData.message;
+
+    data = {
+        email: form.email.value,
+        message: form.message.value
+    };
+    console.log(data);
+}
+
 function onInput() {
 
-    data.email_data = form.elements.email.value;
-    data.message_data = form.elements.message.value;
-
+    data = {
+        email: form.email.value,
+        message: form.message.value
+    };
     localStorage.setItem('feedback-form-state', JSON.stringify(data));
 }
 
-//2
-window.addEventListener('load', onLoad);
-
-function onLoad() {
-    const getformData = JSON.parse(localStorage.getItem('feedback-form-state'));
-
-    if (getformData) {
-        form.elements.email.value = getformData.email_data;
-        form.elements.message.value = getformData.message_data;
-
-        console.log(data);
-    }
-    console.log(data);
-}
 
 //3
 form.addEventListener('submit', onSubmit);
 
-function onSubmit() {
+function onSubmit(event) {
+    event.preventDefault();
+    console.log(data);
     localStorage.removeItem('feedback-form-state');
     reset(form);
-    console.log(data);
 }
-
-
-
-
-
 
